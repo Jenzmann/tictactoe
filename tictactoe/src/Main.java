5117868c33b6391a368d5f44a817210892ba0fb0
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2025-11-28 13:22:45 +0100

    Add unentschieden to stats

--- tictactoe/src/Main.java
+++ tictactoe/src/Main.java
@@ -8,15 +8,13 @@ import tictactoe.spieler.beispiel.Zufallsspieler;
 public static void main(String[] args) {
     ISpieler spieler1 = new Zufallsspieler("Zufall");
 
-    // Fügen Sie nachfolgend Ihren eigenen lernenden Spieler ein.
-    // Dieser muss die Schnittstelle ILernenderSpieler implementieren
-    // Der hier nachfolgende Spieler ist nur ein (nicht funktionierendes) Muster
     ILernenderSpieler spieler2 = new Spieler("Lernender Spieler");
 
     TicTacToe spiel = new TicTacToe();
     ISpieler gewinner;
     int gewinne1 = 0;
     int gewinne2 = 0;
+    int unentschieden = 0;
 
     System.out.println("Vor dem Training");
     System.out.println(spieler1.getName() + " gegen " + spieler2.getName());
@@ -25,29 +23,35 @@ public static void main(String[] args) {
         gewinner = spiel.neuesSpiel(spieler1, spieler2, 150, false);
         if (gewinner == spieler1) {
             gewinne1++;
+        } else if (gewinner == spieler2) {
+            gewinne2++;
         } else {
-            if (gewinner == spieler2)
-                gewinne2++;
+            unentschieden++;
         }
         gewinner = spiel.neuesSpiel(spieler2, spieler1, 150, false);
         if (gewinner == spieler1) {
             gewinne1++;
+        } else if (gewinner == spieler2) {
+            gewinne2++;
         } else {
-            if (gewinner == spieler2)
-                gewinne2++;
+            unentschieden++;
         }
     }
+
     System.out.println(
             "Gewinne " + spieler1.getName() + ": " + gewinne1 + ". Gewinne " + spieler2.getName() + ": " + gewinne2);
     System.out.println();
 
     gewinne1 = 0;
+    unentschieden = 0;
     gewinne2 = 0;
-    // Hier würde jetzt das Training kommen!
+
     System.out.println("Starte Training mit 200000 Iterationen. Bitte haben Sie etwas Geduld!");
+
     long starttime = System.currentTimeMillis();
     spieler2.trainieren(new AbbruchNachIterationen(200000));
     long endtime = System.currentTimeMillis();
+
     System.out.println("Training beendet. Gesamtdauer in Sekunden: " + ((endtime - starttime) / 1000));
     System.out.println(spieler1.getName() + " gegen " + spieler2.getName());
     System.out.println("=======================================================");
@@ -56,21 +60,24 @@ public static void main(String[] args) {
         gewinner = spiel.neuesSpiel(spieler1, spieler2, 150, false);
         if (gewinner == spieler1) {
             gewinne1++;
+        } else if (gewinner == spieler2) {
+            gewinne2++;
         } else {
-            if (gewinner == spieler2)
-                gewinne2++;
+            unentschieden++;
         }
+
         gewinner = spiel.neuesSpiel(spieler2, spieler1, 150, false);
         if (gewinner == spieler1) {
             gewinne1++;
+        } else if (gewinner == spieler2) {
+            gewinne2++;
         } else {
-            if (gewinner == spieler2)
-                gewinne2++;
+            unentschieden++;
         }
     }
 
     System.out.println(
-            "Gewinne " + spieler1.getName() + ": " + gewinne1 + ". Gewinne " + spieler2.getName() + ": " + gewinne2);
+            "Gewinne " + spieler1.getName() + ": " + gewinne1 + ". Gewinne " + spieler2.getName() + ": " + gewinne2 + ". Unentschieden " + unentschieden);
     System.out.println();
     System.out.println("Ein Einzelspiel im DEBUG-Modus, lernender Spieler startet mit X");
     System.out.println("===============================================================");
